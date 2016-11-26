8c2fb4ff8c6ff8c7e5fd559c7cad01091c861699
cosmiczilch <cosmiczilch@gmail.com>
2016-11-26 13:11:06 -0800

    Add MainPanel.

---
+++ Assets/Scripts/MainScene/MainPanel/MainPanel.cs
@@ -0,0 +1,38 @@
+using TimiShared.Debug;
+using TimiShared.Loading;
+using TimiShared.UI;
+using UnityEngine;
+
+namespace TimiTracking.MainScene {
+    public class MainPanel : MonoBehaviour {
+        #region Static
+        private const string kMainPanelPrefabName = "Prefabs/UI/MainPanel";
+        public static void Present() {
+            if (MainPanel.Instance != null) {
+                TimiDebug.LogWarningColor("Already loaded an instance of MainPanel", LogColor.red);
+                return;
+            }
+            PrefabLoader.Instance.InstantiateAsynchronous(kMainPanelPrefabName, UIRootView.Instance.MainCanvas.transform, (loadedGO) => {
+                MainPanel.Instance = loadedGO.GetComponent<MainPanel>();
+                if (MainPanel.Instance == null) {
+                    TimiDebug.LogWarningColor("Cannot find " + typeof(MainPanel).Name + " on loaded prefab", LogColor.red);
+                }
+            });
+        }
+
+        public static MainPanel Instance {
+            get; private set;
+        }
+        #endregion
+
+        #region Unity LifeCycle
+        private void Awake() {
+        }
+
+        private void OnDestroy() {
+            MainPanel.Instance = null;
+        }
+        #endregion
+
+    }
+}
--- Assets/Scripts/MainScene/MainSceneLoader.cs
+++ Assets/Scripts/MainScene/MainSceneLoader.cs
@@ -1,29 +1,16 @@
-using TimiShared.Debug;
-using TimiShared.Loading;
-using TimiShared.UI;
 using UnityEngine;
 
 namespace TimiTracking.MainScene {
     public class MainSceneLoader : MonoBehaviour {
 
-        [SerializeField] private string _mainPanelName;
-
         #region Unity LifeCycle
         private void Start() {
-            this.LoadMainPanelAsync(null);
+            this.LoadMainPanelAsync();
         }
         #endregion
 
-        private void LoadMainPanelAsync(System.Action callback) {
-            if (string.IsNullOrEmpty(this._mainPanelName)) {
-                TimiDebug.LogErrorColor("Main panel not set", LogColor.red);
-            }
-
-            PrefabLoader.Instance.InstantiateAsynchronous(this._mainPanelName, UIRootView.Instance.MainCanvas.transform, (loadedGO) => {
-                if (callback != null) {
-                    callback.Invoke();
-                }
-            });
+        private void LoadMainPanelAsync() {
+            MainPanel.Present();
         }
 
     }
